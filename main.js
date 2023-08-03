d4c76404a324b20e6a1a18ba64e9ab4b27b6c3d0
Neptune669 <89285566+Neptune669@users.noreply.github.com>
2023-08-03 21:57:33 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+
+const links = document.querySelectorAll('nav a');
+
+links.forEach(link => {
+    link.addEventListener('click', function () {
+        links.forEach(link => link.classList.remove('active'));
+        this.classList.add('active');
+    });
+});
